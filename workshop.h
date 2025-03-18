ca571dd6d8277cbe110f2258448d80f7494f8179
zerbco <federicoektn09@gmail.com>
2025-03-18 21:13:52 +1030

    Add files via upload

---
+++ workshop.h
@@ -0,0 +1,10 @@
+#ifndef WORKSHOP_H
+#define WORKSHOP_H
+
+void changeValue(double *ptr);
+void printArray(double *array, int n);
+double arrayMax(double *array, int n);
+double* dynamicArray(int N);
+double* dynamicArray(int N, double M);
+
+#endif
